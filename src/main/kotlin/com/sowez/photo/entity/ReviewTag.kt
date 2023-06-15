2ca680c6b3d9d19652e5bf23b1b4e816984b2c3c
Eumji <grapeti200@gmail.com>
2023-06-16 00:59:06 +0900

    feat #6 - ReviewTag 엔티티 구현

--- src/main/kotlin/com/sowez/photo/entity/ReviewTag.kt
+++ src/main/kotlin/com/sowez/photo/entity/ReviewTag.kt
@@ -1,4 +1,23 @@
 package com.sowez.photo.entity
 
-class ReviewTag {
+import jakarta.persistence.*
+
+@Entity
+class ReviewTag (
+    review: Review,
+    tag: Tag
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, name = "review_tag_id")
+    val id: Long = 0
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, name = "review_id")
+    var review: Review = review
+        protected set
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, name = "tag_id")
+    var tag: Tag = tag
+        protected set
 }
