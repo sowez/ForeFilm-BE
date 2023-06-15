4efeba84ab0148528b89cfd0ea01e5bbea87a3cf
Eumji <grapeti200@gmail.com>
2023-06-16 00:57:07 +0900

    feat #6 - ReviewImage 엔티티 구현

--- src/main/kotlin/com/sowez/photo/entity/ReviewImage.kt
+++ src/main/kotlin/com/sowez/photo/entity/ReviewImage.kt
@@ -1,4 +1,23 @@
 package com.sowez.photo.entity
 
-class ReviewImage {
+import jakarta.persistence.*
+
+@Entity
+class ReviewImage (
+    review: Review,
+    image: Image
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, name = "review_image_id")
+    val id: Long = 0
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, name = "review_id")
+    var review: Review = review
+        protected set
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, name = "image_id")
+    var image: Image = image
+        protected set
 }
