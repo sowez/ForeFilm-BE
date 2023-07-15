d64c61f86d644e4db2a229770b2e2f8cfd9d3e3e
jjungyeun <wjddusdl11@ajou.ac.kr>
2023-07-15 17:29:49 +0900

    fix #19 - Enum 타입에 대한 validation 어노테이션 수정
    - Enum 필드는 @NotBlank를 적용할 수 없으므로 @NotNull로 변경

--- src/main/kotlin/com/sowez/photo/dto/req/StoreRequestDto.kt
+++ src/main/kotlin/com/sowez/photo/dto/req/StoreRequestDto.kt
@@ -9,7 +9,7 @@ import jakarta.validation.constraints.NotNull
 data class StoreCreateReqDto(
     @field:NotBlank
     val storeName: String,
-    @field:NotBlank
+    @field:NotNull
     val storeType: StoreType,
     @field:NotBlank
     val storeAddress: String,
