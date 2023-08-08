952096da375346c87d1b4e83773927eaba23fc7b
jjungyeun <wjyddd@naver.com>
2023-08-08 17:54:43 +0900

    test #19 - 사진관 API 테스트 코드 작성 완료
    - StoreController API 테스트 코드 작성

--- src/test/kotlin/com/sowez/photo/controller/StoreControllerTest.kt
+++ src/test/kotlin/com/sowez/photo/controller/StoreControllerTest.kt
@@ -2,6 +2,7 @@ package com.sowez.photo.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.sowez.photo.dto.req.StoreCreateReqDto
+import com.sowez.photo.dto.req.StoreEditReqDto
 import com.sowez.photo.type.PayType
 import com.sowez.photo.type.StoreType
 import org.junit.jupiter.api.DisplayName
@@ -11,7 +12,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
@@ -46,6 +47,167 @@ class StoreControllerTest(
                 .andExpect(status().isOk)
                 .andExpect(jsonPath("$.body.created").value(1L))
                 .andDo(print())
+    }
+
+    @Test
+    @DisplayName("새로운 매장 생성 (필수 항목만 포함)")
+    fun create_new_store_with_only_required_fields() {
+        // given
+        val requestDto = StoreCreateReqDto(
+                storeName = "하루필름 강남점",
+                storeType = StoreType.STORE,
+                storeAddress = "서울 어쩌구 저쩌구",
+                brandId = 11L,
+                storeOperatingTime = null,
+                storePhoneNum = null,
+                payTypes = null
+        )
+
+        // when & then
+        mockMvc.perform(
+                post("/stores")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestDto))
+        )
+                .andExpect(status().isOk)
+                .andExpect(jsonPath("$.body.created").value(1L))
+                .andDo(print())
+    }
+
+    @Test
+    @DisplayName("매장 정보 수정")
+    fun edit_store_info() {
+        // given
+        val requestDto = StoreEditReqDto(
+            storeName = "하루필름 강남점",
+            storeType = StoreType.STORE,
+            storeAddress = "서울 어쩌구 저쩌구",
+            brandId = 11L,
+            storeOperatingTime = "24시간 영업",
+            storePhoneNum = "010-1234-5678",
+            payTypes = listOf(PayType.CARD, PayType.CASH)
+        )
+
+        // when & then
+        mockMvc.perform(
+            patch("/stores/{storeId}", 1L)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(requestDto))
+        )
+            .andExpect(status().isOk)
+            .andDo(print())
+    }
+
+    @Test
+    @DisplayName("매장 정보 수정 (필수 항목만 포함)")
+    fun edit_store_info_with_only_required_fields() {
+        // given
+        val requestDto = StoreEditReqDto(
+            storeName = "하루필름 강남점",
+            storeType = StoreType.STORE,
+            storeAddress = "서울 어쩌구 저쩌구",
+            brandId = 11L,
+            storeOperatingTime = null,
+            storePhoneNum = null,
+            payTypes = null
+        )
+
+        // when & then
+        mockMvc.perform(
+            patch("/stores/{storeId}", 1L)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(requestDto))
+        )
+            .andExpect(status().isOk)
+            .andDo(print())
+    }
+
+    @Test
+    @DisplayName("매장 정보 조회")
+    fun get_store_info() {
+        // when & then
+        mockMvc.perform(
+            get("/stores/{storeId}", 1L)
+        )
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.body.store_name").value("하루필름 강남점"))
+            .andExpect(jsonPath("$.body.store_type").value(StoreType.STORE.name))
+            .andExpect(jsonPath("$.body.store_address").value("서울 어쩌구 저쩌구"))
+            .andExpect(jsonPath("$.body.brand_id").value(11L))
+            .andExpect(jsonPath("$.body.store_operating_time").value("24시간 영업"))
+            .andExpect(jsonPath("$.body.store_phone_num").value("010-1234-5678"))
+            .andExpect(jsonPath("$.body.pay_types[0]").value(PayType.CARD.name))
+            .andExpect(jsonPath("$.body.pay_types[1]").value(PayType.CASH.name))
+            .andDo(print())
+    }
+
+    @Test
+    @DisplayName("매장 검색")
+    fun search_store() {
+        // when & then
+        mockMvc.perform(
+            get("/stores?q=강남")
+        )
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.body.content[0].store_id").value(1))
+            .andExpect(jsonPath("$.body.content[0].store_name").value("하루필름 강남점"))
+            .andExpect(jsonPath("$.body.content[0].store_address").value("여기저기"))
+            .andExpect(jsonPath("$.body.content[0].review_cnt").value(101))
+            .andExpect(jsonPath("$.body.content[1].store_id").value(4))
+            .andExpect(jsonPath("$.body.content[1].store_name").value("하루필름 강남2호점"))
+            .andExpect(jsonPath("$.body.content[1].store_address").value("요기조기"))
+            .andExpect(jsonPath("$.body.content[1].review_cnt").value(79))
+            .andDo(print())
+    }
+
+    @Test
+    @DisplayName("매장의 브랜드 이미지 조회")
+    fun get_store_brand_image() {
+        // when & then
+        mockMvc.perform(
+            get("/stores/{storeId}/brand-logo-image", 1L)
+        )
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.body.brand_id").value(10L))
+            .andExpect(jsonPath("$.body.brand_logo_image_id").value(100L))
+            .andExpect(jsonPath("$.body.image_url").value("https://www.forefilm.com/images/123"))
+            .andDo(print())
+    }
+
+    @Test
+    @DisplayName("매장 사진 리스트 조회")
+    fun get_store_images() {
+        // when & then
+        mockMvc.perform(
+            get("/stores/{storeId}/images?limit=20&offset=1", 1L)
+        )
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.body.images[0].image_id").value(1L))
+            .andExpect(jsonPath("$.body.images[0].image_url").value("https://www.forefilm.com/images/1234"))
+            .andExpect(jsonPath("$.body.images[1].image_id").value(5L))
+            .andExpect(jsonPath("$.body.images[1].image_url").value("https://www.forefilm.com/images/4321"))
+            .andExpect(jsonPath("$.body.images[2].image_id").value(10L))
+            .andExpect(jsonPath("$.body.images[2].image_url").value("https://www.forefilm.com/images/43"))
+            .andExpect(jsonPath("$.body.last_image_id").value(10L))
+            .andDo(print())
+    }
+
+    @Test
+    @DisplayName("매장 사진 리스트 조회 (limit, offset 정보 없음)")
+    fun get_store_images_no_param() {
+        // when & then
+        mockMvc.perform(
+            get("/stores/{storeId}/images", 1L)
+        )
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.body.images[0].image_id").value(1L))
+            .andExpect(jsonPath("$.body.images[0].image_url").value("https://www.forefilm.com/images/1234"))
+            .andExpect(jsonPath("$.body.images[1].image_id").value(5L))
+            .andExpect(jsonPath("$.body.images[1].image_url").value("https://www.forefilm.com/images/4321"))
+            .andExpect(jsonPath("$.body.images[2].image_id").value(10L))
+            .andExpect(jsonPath("$.body.images[2].image_url").value("https://www.forefilm.com/images/43"))
+            .andExpect(jsonPath("$.body.last_image_id").value(10L))
+            .andDo(print())
     }
 
 }
