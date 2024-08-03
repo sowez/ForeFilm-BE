4908c5194e3b67704a47bf2faeddfc356203ad23
jjungyeun <wjyddd@naver.com>
2024-08-03 16:52:27 +0900

    fix #34 - 레이아웃 관련 클래스 삭제
    - 엔티티, 레포지토리 삭제

--- src/main/kotlin/com/sowez/photo/repository/StoreLayoutRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.sowez.photo.repository
-
-import com.sowez.photo.entity.StoreLayout
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface StoreLayoutRepository: JpaRepository<StoreLayout, Long> {
-}
